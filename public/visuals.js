0dc99961613d5e2c0de7e17e9b8bbb6811e4e93b
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-04-25 10:25:59 -0500

    Hellow World

---
+++ public/visuals.js
@@ -0,0 +1,20 @@
+var data = [80, 120, 60, 150, 200];
+var barHeight = 20;
+var bar = d3.select('svg')
+  .selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('width', function (d) { return d; })
+  .attr('height', barHeight - 1)
+  .attr('transform', function (d, i) {
+    return "translate(0," + i * barHeight + ")";
+  })
+  .style('color', 'orange')
+
+d3.select('button')
+  .on('click', function () {
+    d3.select('body')
+      .append('h3')
+      .text('Today is a beautiful day!!');
+  });
